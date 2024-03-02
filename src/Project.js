7d80e3cc9557bb07fc05c1f0441ec7127f089af6
Prabhpreet Walia <prabhpreet.business@gmail.com>
2024-03-02 23:50:14 +0530

    Links added

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,7 @@
 import "./Project.css";
 import projects from "./Project.json";
+import Github from "./assets/Github";
+import CustomLink from "./assets/CustomLink"
 
 export default function Project() {
     let appends = [];
@@ -7,7 +9,6 @@ export default function Project() {
     appends = projects.map((project) => {
         return (
             <>
-                
                 <div className="project" key={project.projectID}>
                     <div className="project--gallery">
                         {project.img1 && (
@@ -21,16 +22,32 @@ export default function Project() {
                         )}
                     </div>
                     <div className="project--text">
+                        <a
+                            className="about--heading project--heading"
+                            href={project.link}
+                            target="_blank"
+                        >
+                            {project.title}
+                        </a>
 
-                        <a className="about--heading project--heading" href={project.link} target="_blank">{project.title}</a>
-                        
                         <div className="about--subheading project--subheading">
                             {project.sub}
                         </div>
                         <div className="about--paragraph">{project.about}</div>
                         <br />
                         <div className="project--stack">
-                            <span>Stack Used | </span><img src={"https://skillicons.dev/icons?i=" + project.stack} />
+                            <span>Stack Used | </span>
+                            <img src={"https://skillicons.dev/icons?i=" + project.stack} />
+                        </div>
+                        <div className="hero--contact project--link">
+                            <a href={project["github-link"]} target="_blank">
+                                <Github /> <span>Get Code</span>
+                            </a>
+                            <br />
+                            <a href={project["demo-link"]} target="_blank">
+                                <CustomLink /> <span>Live Demo</span>
+                            </a>
+
                         </div>
                     </div>
                 </div>
@@ -40,10 +57,10 @@ export default function Project() {
 
     return (
         <>
-        <div className="top-heading" id="project">Check out my projects... <span className="hand-down">👇</span></div>
-        <div className="Project--container">
-        {appends}
-        </div>
+            <div className="top-heading" id="project">
+                Check out my projects... <span className="hand-down">👇</span>
+            </div>
+            <div className="Project--container">{appends}</div>
         </>
     );
 }
